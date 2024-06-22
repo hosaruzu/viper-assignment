a98f3112d1217424773a276bc56bbffaf0144f46
Artem Tebenkov <artem.tebenkov@icloud.com>
2024-06-22 05:58:17 +0400

    Create project

---
+++ viper-assignment/viper-assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  viper-assignment
+//
+//  Created by Artem Tebenkov on 22.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
