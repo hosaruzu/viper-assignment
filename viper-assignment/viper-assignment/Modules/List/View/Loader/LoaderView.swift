976ec2649281244f117319528fa67d93d99a32d7
Artem Tebenkov <artem.tebenkov@icloud.com>
2024-06-28 13:46:00 +0400

    Fix public methods in loader view

--- viper-assignment/viper-assignment/Modules/List/View/Loader/LoaderView.swift
+++ viper-assignment/viper-assignment/Modules/List/View/Loader/LoaderView.swift
@@ -26,18 +26,18 @@ final class LoaderView: UIView {
         setupConstraints()
         start()
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
     // MARK: - Public
 
-    private func start() {
+    func start() {
         activityIndicator.startAnimating()
     }
 
-    private func stop() {
+    func stop() {
         activityIndicator.stopAnimating()
     }
 }
