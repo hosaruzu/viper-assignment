3c9c03d93c30b9812e18e4dc9bf3bd73ebd1cb0b
Artem Tebenkov <artem.tebenkov@icloud.com>
2024-06-28 17:31:14 +0400

    Create detail view

--- viper-assignment/viper-assignment/Modules/Detail/View/DetailViewController.swift
+++ viper-assignment/viper-assignment/Modules/Detail/View/DetailViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 final class DetailViewController: UIViewController {
 
+    // MARK: - View output
+
     var output: DetailViewOutput!
+
+    // MARK: - Subviews
+
+    private let detailView = DetailView()
+
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         output.viewDidLoad()
+        setupAppearance()
+        setupSubviews()
+        setupConstraints()
+    }
+}
+
+// MARK: - Setup appearance
+
+private extension DetailViewController {
+    func setupAppearance() {
+        view.backgroundColor = .systemBackground
+        title = "Item name"
+    }
+}
+
+// MARK: - Setup layout
+
+private extension DetailViewController {
+    func setupSubviews() {
+        view.addSubview(detailView)
+    }
+
+    func setupConstraints() {
+        detailView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
     }
 }
 
