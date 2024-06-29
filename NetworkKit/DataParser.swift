93ca3cd93abb47016d0c7bd302a6c2583779797c
Artem Tebenkov <artem.tebenkov@icloud.com>
2024-06-29 05:46:59 +0400

    Create DataParser

---
+++ NetworkKit/DataParser.swift
@@ -0,0 +1,24 @@
+//
+//  DataParser.swift
+//  NetworkKit
+//
+//  Created by Artem Tebenkov on 29.06.2024.
+//
+
+protocol DataParserProtocol {
+    func parse<T: Decodable>(data: Data) throws -> T
+}
+
+final class DataParser: DataParserProtocol {
+
+    private var jsonDecoder: JSONDecoder
+
+    init(jsonDecoder: JSONDecoder) {
+        self.jsonDecoder = jsonDecoder
+        self.jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+    }
+
+    func parse<T: Decodable>(data: Data) throws -> T {
+        return try jsonDecoder.decode(T.self, from: data)
+    }
+}
