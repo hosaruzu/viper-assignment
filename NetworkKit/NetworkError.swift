f2a19f7e43b374f40fac0cb657a075b70ae90db0
Artem Tebenkov <artem.tebenkov@icloud.com>
2024-06-29 05:46:47 +0400

    extend NetworkError with description

--- NetworkKit/NetworkError.swift
+++ NetworkKit/NetworkError.swift
@@ -5,7 +5,16 @@
 //  Created by Artem Tebenkov on 29.06.2024.
 //
 
-enum NetworkError: Error {
+enum NetworkError: Error, CustomStringConvertible {
     case invalidURL
     case invalidServerResponse
+
+    var description: String {
+        switch self {
+        case .invalidURL:
+            "URL string in malformed."
+        case .invalidServerResponse:
+            "The server returned an invalid response."
+        }
+    }
 }
