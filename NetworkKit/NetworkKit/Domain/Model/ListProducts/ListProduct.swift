f410a7260df46f69a8417de7857d18891f0ed640
Artem Tebenkov <artem.tebenkov@icloud.com>
2024-07-02 13:24:15 +0400

    Prepare mock data for Unit testing

--- NetworkKit/NetworkKit/Domain/Model/ListProducts/ListProduct.swift
+++ NetworkKit/NetworkKit/Domain/Model/ListProducts/ListProduct.swift
@@ -13,3 +13,19 @@ public struct ListProduct: Decodable {
     public let imageUrl: String
     public let createdDate: String
 }
+
+extension ListProduct {
+    static let mock = loadProductsList()
+}
+
+private struct ListProductMock: Decodable {
+    let products: [ListProduct]
+}
+private func loadProductsList() -> [ListProduct] {
+    guard let url = Bundle.main.url(forResource: "ListProductsMock", withExtension: "json"),
+          let data = try? Data(contentsOf: url) else { return [] }
+    let decoder = JSONDecoder()
+    decoder.keyDecodingStrategy = .convertFromSnakeCase
+    let jsonMock = try? decoder.decode(ListProductMock.self, from: data)
+    return jsonMock?.products ?? []
+}
